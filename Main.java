84512723c7a05220541c2f8821b63439dd807316
Mizuki Torii <m.torii.sys21@morijyobi.ac.jp>
2022-03-17 11:53:35 +0900

    コミット

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args ){
+        double height, weight, bmi;
+
+        height = 160;
+
+        weight = 50;
+
+        bmi = weight / height / height;
+
+         System.out.println("BMI is " + bmi);
+
+    }
+}
